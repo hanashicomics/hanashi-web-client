da00b73f3b0fa71847fac9a8bcb20f3bd591c984
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-06-26 23:15:43 +0200

    Add delete functionality with confirmation modal in Arcs component
    Fixes Issue #17 :Users can delete arcs

--- src/components/Arcs.jsx
+++ src/components/Arcs.jsx
@@ -2,13 +2,14 @@ import StoryFooterNavigation from "./StoryFooterNavigation.jsx";
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import '../assets/styles/Arcs.css';
-import {getStoryByTitle} from "../lib/db.js";
+import {getStoryByTitle, updateStory} from "../lib/db.js";
 import ConfirmModal from "../modals/ConfirmModal.jsx";
 import MessageModal from "../modals/MessageModal.jsx";
 
 export default function Arcs(){
     const {storyName} = useParams();
     const[arcs, setArcs] = useState([]);
+    const[arcNameToBeDeleted, setarcNameToBeDeleted] = useState("");
     const [showConfirm, setShowConfirm] = useState(false);
     const[keyTitle, setKeyTitle] = useState({});
     const [modalOpen, setModalOpen] = useState(false);
@@ -19,7 +20,13 @@ export default function Arcs(){
 
     const handleConfirm = async () => {
         setShowConfirm(false);
-        //await handleIdbDelete(keyTitle.id,keyTitle.title);
+        const updatedArr = arcs.filter(arc => arc.name !== arcNameToBeDeleted);
+        setArcs(updatedArr);
+        updateStory({
+            ...await getStoryByTitle(storyName),
+            title: storyName,
+            arcs: updatedArr
+        })
         setModalOpen(true)
     };
 
@@ -48,7 +55,12 @@ export default function Arcs(){
                     + Create New Arc
                 </Link>
             </div>
-
+            <ConfirmModal
+                isOpen={showConfirm}
+                message="Are you sure you want to delete this arc? This action cannot be undone.!"
+                onConfirm={handleConfirm}
+                onCancel={handleCancel}
+            />
             <div className="arcList">
                 {
                     arcs.length < 1 ? (
@@ -59,7 +71,18 @@ export default function Arcs(){
                                 <div className="arcCard">
                                     <h4>{arc.name}</h4>
                                 </div>
+                                <button
+                                    className="delete-btn"
+                                    onClick={async (e) => {
+                                        e.preventDefault();
+                                        setarcNameToBeDeleted(arc.name);
+                                        handleDelete();
+                                    }}
+                                >
+                                    Delete
+                                </button>
                             </Link>
+
                         ))
                     )
                 }
